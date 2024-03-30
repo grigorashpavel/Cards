b63e6ee41a0e83f0ccd116b0cac23d07b8e77d47
grigorash.pasha <grigorash.pavel.it@yandex.ru>
2024-03-30 22:05:38 +0300

    Switch UI to Constraints

--- features/sign-up/src/main/java/com/pasha/sign_up/SignUpFragment.kt
+++ features/sign-up/src/main/java/com/pasha/sign_up/SignUpFragment.kt
@@ -1,16 +1,20 @@
 package com.pasha.sign_up
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.textview.MaterialTextView
 
 class SignUpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        val view =  inflater.inflate(R.layout.fragment_sign_up, container, false)
+        view.findViewById<MaterialTextView>(R.id.footer).movementMethod = LinkMovementMethod.getInstance()
+        return view
     }
 }
